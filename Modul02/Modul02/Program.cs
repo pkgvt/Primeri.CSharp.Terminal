d5d1bf6013e54c14a861f671f9a7265a4587bffe
Petar Georgiev <pkg_vt@abv.bg>
2015-09-07 20:27:21 +0300

    Модул 02, Видио 01, Добавяне на коментари

---
+++ Modul02/Modul02/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Modul02
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+				
+			//Дефиниране на променливи
+
+			//Събиране на числа
+
+			//Писане в конзола
+		}
+	}
+}
