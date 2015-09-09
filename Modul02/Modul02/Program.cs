0a6dd3bce0769ade08ccb15d07668f0453bc6609
Petar Georgiev <pkg_vt@abv.bg>
2015-09-09 20:12:59 +0300

    Модул 02, Видео 03, Основни оператори за целенасочен тип данни

--- Modul02/Modul02/Program.cs
+++ Modul02/Modul02/Program.cs
@@ -11,8 +11,8 @@ namespace Modul02
 			int a = 0, b = 0;
 			int sum = 0;
 			//Събиране на числа
-			a = 15;
-			b = 50;
+			a = 10;
+			b = 5;
 			sum = a + b;
 			//Писане в конзола
 			Console.Write ( a );
@@ -20,6 +20,29 @@ namespace Modul02
 			Console.Write ( b );
 			Console.Write (" = " );
 			Console.WriteLine ( sum );
+
+			//Допълнителни оператори
+			Console.WriteLine (" \n Изпозване на +=");
+			a += b; //a =10 +5, a = a+b
+			Console.WriteLine ( a );
+
+			Console.WriteLine (" \n Изпозване на -=");
+			a -= b; 
+			Console.WriteLine ( a );
+
+			Console.WriteLine (" \n Изпозване на *=");
+			a *= b; 
+			Console.WriteLine ( a );
+
+			Console.WriteLine (" \n Изпозване на ++");
+			a++; 
+			Console.WriteLine ( a );
+
+			Console.WriteLine ("Стандартно делене:" +(14/4).ToString()); //->2 (7/3=2,33333)
+				
+			Console.WriteLine ("Остатък от делене:" +(14%4).ToString()); //->1 (1/3=0,3333)
+
+
 		}
 	}
 }
