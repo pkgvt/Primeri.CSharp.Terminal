8b5cb984386f945931c7d22923d5717bf9edc106
Petar Georgiev <pkg_vt@abv.bg>
2015-09-12 19:32:55 +0300

    Модул 02, Видео 08, Двумерни масиви

--- Modul02/TableProject/Program.cs
+++ Modul02/TableProject/Program.cs
@@ -6,24 +6,36 @@ namespace TableProject
 	{
 		public static void Main (string[] args)
 		{
+			// Дефиниране таблица
+			string [ , ] table = new string [2,3];
 
-			string[] row = new string[3];
+			//Въвеждане на първи ред
 
-			row [0] = "Ред 1";
-			row [1] = "Ред 2";
-			row [2] = "Ред 3";
+			table [0,0] = "Мартин";table [0,1] = "Стоев";table [0,2] = "31";
+			//Въвеждане на първи ред
+			table [1,0] = "Симеон";table [1,1] = "Мартинов";table [1,2] = "41";
 
+			//Печат
+			Console.Write("Кой ред искате да видит:");
+			int _index = Convert.ToInt32 (Console.ReadLine ()) - 1;
 
-			Console.WriteLine ("Масив row: " +row [0] + "," + row [1] + "," + row [2] + "\n");
-
-			string[] parse = "1,2,3,4,5,6,7,8,9".Split(',');
-
-			Console.WriteLine ("Броят на масива:\n " + "1,2,3,4,5,6,7,8,9\n\nе:" + parse.Length);
-
-			string list1 = string.Join (";", parse);
-
-			Console.WriteLine ("новият стринг е:\n" + list1 + "\n\n");
-
+			Console.WriteLine((table[_index,0])[0] + "." + table[_index,1]+ "," + table[_index,2] + "г.");
 		}
 	}
 }
+//			string[] row = new string[3];
+//
+//			row [0] = "Ред 1";
+//			row [1] = "Ред 2";
+//			row [2] = "Ред 3";
+//
+//
+//			Console.WriteLine ("Масив row: " +row [0] + "," + row [1] + "," + row [2] + "\n");
+//
+//			string[] parse = "1,2,3,4,5,6,7,8,9".Split(',');
+//
+//			Console.WriteLine ("Броят на масива:\n " + "1,2,3,4,5,6,7,8,9\n\nе:" + parse.Length);
+//
+//			string list1 = string.Join (";", parse);
+//
+//			Console.WriteLine ("новият стринг е:\n" + list1 + "\n\n");
