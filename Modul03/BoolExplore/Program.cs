a263a14f2189ecc0a93d6ac5deb988604da56ed9
Petar Georgiev <pkg_vt@abv.bg>
2015-09-19 15:13:42 +0300

    Модул о3, Видео 06, Условни оператори if

--- Modul03/BoolExplore/Program.cs
+++ Modul03/BoolExplore/Program.cs
@@ -13,20 +13,38 @@ namespace BoolExplore
 
 			int _ui = Convert.ToInt32 (Console.ReadLine ());
 
-			bool _check01 = _ui <= _i.Length;
+//			bool _check01 = _ui <= _i.Length;
+//
+//			bool _check02 = _ui > 0;
+//
+//
+//			bool _exp01 = _check01 && _check02; //Логическо И "&&"
+//			bool _exp02 = _check02 || _check02; //Логическо ИЛИ "||"
+//			bool _exp03 = !_check01; //Отрицание "!"
+//
+//			Console.WriteLine ("Логическо И :" + _exp01.ToString ());
+//			Console.WriteLine ("Логическо ИЛИ :" + _exp02.ToString ());
+//			Console.WriteLine ("Отрицание на" + _check01.ToString () + " : " + _exp03);
 
-			bool _check02 = _ui > 0;
+			if (_ui > 0 && _ui <= _i.Length)
+			{
 
+				Console.WriteLine (_i [_ui - 1]);
+			} else {
+				if (_ui > _i.Length)
+				{
+					Console.WriteLine ("Индексът е прекалено голям \n\n\n");
+				}
+		    if (_ui <= 0 )
 
-			bool _exp01 = _check01 && _check02; //Логическо И "&&"
-			bool _exp02 = _check02 || _check02; //Логическо ИЛИ "||"
-			bool _exp03 = !_check01; //Отрицание "!"
+				{
+				Console.WriteLine ("Индексът е прекалено малък \n\n\n");
+                }
 
-			Console.WriteLine ("Логическо И :" + _exp01.ToString ());
-			Console.WriteLine ("Логическо ИЛИ :" + _exp02.ToString ());
-			Console.WriteLine ("Отрицание на" + _check01.ToString () + " : " + _exp03);
-
-			//Console.WriteLine ( _i [ _ui-1]);
 		}
+
+	}
+
 	}
 }
+
