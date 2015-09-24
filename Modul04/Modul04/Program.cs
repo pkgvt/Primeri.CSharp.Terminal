5b885f042b735acdc58d045873020d6fbac9a241
Petar Georgiev <pkg_vt@abv.bg>
2015-09-24 18:36:30 +0300

    Модул 04, Видео 01, Цикъл for

---
+++ Modul04/Modul04/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Modul04
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			for (int i=0; i <=5; i +=2)
+
+				//Код за цикъла
+
+				Console.WriteLine (i.ToString ());
+		}
+	}
+}
