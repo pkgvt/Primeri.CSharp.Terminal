7477e952d602c651b0ae10a839a96c957a847dc2
Petar Georgiev <pkg_vt@abv.bg>
2015-09-29 15:40:51 +0300

    Модул 04, Видео 04, Цикъл while

--- Modul04/Modul04/Program.cs
+++ Modul04/Modul04/Program.cs
@@ -20,22 +20,36 @@ namespace Modul04
 			// Анкета на таблица
 			Console.WriteLine ("Име".PadRight(12)+"Фамилия".PadRight(12)+"Години".PadRight(12) );
 			Console.WriteLine ("=================================");
+		
 
+
+			//Пример с for
 			// Тяло на таблицата
 
-			for (int i = 0; i < _table.Length/3; i++) 
+//			for (int i = 0; i < _table.Length/3; i++) 
+//			{
+//				for (int j = 0; j < 3; j++) 
+//				{
+//					
+//					Console.Write ( _table [i, j].PadRight (12) );
+//				}
+//				Console.WriteLine ();
+
+			// Пример с while
+
+			int i = 0;
+			while (i < _table.Length / 3)
 			{
-				for (int j = 0; j < 3; j++) 
-				{
-					
-					Console.Write ( _table [i, j].PadRight (12) );
-				}
-				Console.WriteLine ("\n");
+				Console.WriteLine (
+					_table[i,0].PadRight(12)+
+					_table[i,1].PadRight(12)+
+					_table[i,2].PadRight(12) 
+				);
 
-
+				i++;
 			}
+
 			Console.WriteLine ("=================================");
-	
 		}
 	}
 }  
