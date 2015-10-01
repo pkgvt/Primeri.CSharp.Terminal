5db8dd901593414d81658df94dc6ef8410e7364c
Petar Georgiev <pkg_vt@abv.bg>
2015-10-01 17:38:52 +0300

    Модул04, Видео 06, Примери с do...while

---
+++ Modul04/ListExplore/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ListExplore
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			//Дефиниции
+
+			string _userinput =" ";
+					
+		// List
+
+        // Потребителски вхов
+			do {
+				Console.Write ("? ");
+				_userinput = Console.ReadLine ();
+
+				// Добавяне на стойност
+
+				// Преглед на List
+
+
+				// Размер на List
+		
+			} while(_userinput != "exit"); 	
+		}
+	}
+}
