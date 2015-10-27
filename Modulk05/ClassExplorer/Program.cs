f2d900f123e4ed012114564a263c1bfb9449c41c
Petar Georgiev <pkg_vt@abv.bg>
2015-10-27 20:49:51 +0200

    Модул 05, Видео 08 Рекурсивни методи

--- Modulk05/ClassExplorer/Program.cs
+++ Modulk05/ClassExplorer/Program.cs
@@ -4,61 +4,90 @@ namespace ClassExplorer
 {
 	public class person
 	{
-		private string simpleString ="тестови променливи от клас";
-		private string user = "";
-
-		private string hiddenString = "";
-		public person(string _ini)
-		{
-			getUser (_ini);
-			setWelcomeTExt ();
-
-
-
-
-
-		}
-
-		private void getUser(string _ini)
-		{
-
-			if (_ini == "password1") hiddenString = "Потребител1";
-			if (_ini == "password2") hiddenString = "Потребител2";
-
-			user=hiddenString;
-		}
-
-		private void setWelcomeTExt()
-		{
 		
-			if (hiddenString.Length>0)
+		private string _userName ="";
+		private string _userPass ="";
 
-				simpleString = "Добре дошли отново,  "+ user + "!";
-			else
-				simpleString = "Вие нямате достъп до тази програма.";	
-		}
-		public string getWelcomeMessage()
+		private string _userID ="";
+		private string _userFName ="";
+		private string _userLName ="";
+
+		public string userName
 		{
-			return simpleString;
-
+			get { return _userName;}
+		}
+		public string userPass
+		{
+			get { return _userPass;}
 		}
 
+
+				public string userID
+		{
+
+			get { return _userID; }
+			set { _userID = value; }
 		}
 
+		public string userFName
+
+		{
+
+			get { return _userFName; }
+			set { _userFName = value; }
+		}
+
+		public string userLName
+		{
+
+			get { return _userLName; }
+			set { _userLName = value; }
+		}
+
+		public person(string _user, string _pass)
+		{
+			
+			checkUser (_user, _pass);
+
+		}
+	
+		private bool checkUser(string _user, string _pass)
+		{
+			if (_user == "user" && _pass == "pass") {
+				_userName = _user; _userPass = _pass;
+
+
+				//Попълване на пропартита
+
+				userID = "ID:4455855";
+				userFName = "Petar";
+				userLName = "Georgiev";
+
+				return true;
+			}
+
+			return false;
+	
+		}
+	}	
 	class MainClass
 	{
 		
 		public static void Main (string[] args)
 		{
 
-			Console.Write ("Парола: "); string _user = Console.ReadLine ();
-
+			Console.Write ("Потребител: "); string _user = Console.ReadLine ();
+			Console.Write ("Парола: ");     string _pass = Console.ReadLine ();
 
 			//Дефиниране на клас
-			person _person = new person (_user);
+			person _person = new person ( _user, _pass);
 
 			//Достъпване на клас
-			Console.WriteLine (_person.getWelcomeMessage());
+			_person.userFName ="Първо име";
+				Console.WriteLine (_person.userFName + " " + _person.userLName);
+//			Console.WriteLine (_person.getWelcomeMessage());
 		}
+			
+		}
+
 	}
-}
---
+++ Modulk05/Fact/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Fact
+{
+	class MainClass
+	{
+
+
+		public static int Fact ( int _fact )
+		{
+			if(_fact<=1)
+				return 1;
+			else
+				return _fact* Fact(_fact-1);
+
+		}
+
+
+		public static void Main (string[] args)
+		{
+			Console.WriteLine (Fact(3));
+			Console.WriteLine (Fact(4));
+			Console.WriteLine (Fact(5));
+			Console.WriteLine (Fact(6));
+			Console.WriteLine (Fact(7));
+			Console.WriteLine (Fact(8));
+			Console.WriteLine (Fact(9));
+			Console.WriteLine (Fact(10));
+
+		}
+	}
+}
